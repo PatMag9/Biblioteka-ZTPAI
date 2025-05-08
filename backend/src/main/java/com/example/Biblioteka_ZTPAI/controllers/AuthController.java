c1cfe742a366cee64db03228542b12d7f9a52e5d
PatMag9 <patryk.magiera@student.pk.edu.pl>
2025-05-08 14:22:45 +0200

    feat: implement registration

---
+++ backend/src/main/java/com/example/Biblioteka_ZTPAI/controllers/AuthController.java
@@ -0,0 +1,30 @@
+package com.example.Biblioteka_ZTPAI.controllers;
+
+import com.example.Biblioteka_ZTPAI.models.AuthenticationResponse;
+import com.example.Biblioteka_ZTPAI.models.LoginRequest;
+import com.example.Biblioteka_ZTPAI.models.RegisterRequest;
+import com.example.Biblioteka_ZTPAI.services.AuthService;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+}
---
+++ backend/src/main/java/com/example/Biblioteka_ZTPAI/services/AuthService.java
@@ -0,0 +1,46 @@
+package com.example.Biblioteka_ZTPAI.services;
+
+import com.example.Biblioteka_ZTPAI.configs.JwtService;
+import com.example.Biblioteka_ZTPAI.models.*;
+import com.example.Biblioteka_ZTPAI.repositories.RoleRepository;
+import com.example.Biblioteka_ZTPAI.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public AuthService(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, JwtService jwtService, AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = new User(
+                request.getUsername(),
+                request.getEmail(),
+                passwordEncoder.encode(request.getPassword()));
+
+        Role userRole = roleRepository.findByRoleName("USER")
+                .orElseThrow(() -> new RuntimeException("Rola 'USER' nie została znaleziona"));
+
+        user.addRole(userRole);
+
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return new AuthenticationResponse(jwtToken);
+    }
+
+}
