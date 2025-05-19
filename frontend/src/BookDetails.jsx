57521fe87071a746a02e78486f3365bf52be7c73
PatMag9 <patryk.magiera@student.pk.edu.pl>
2025-05-19 15:10:24 +0200

    feat: modify book details page so it uses JWT authorization

--- frontend/src/BookDetails.jsx
+++ frontend/src/BookDetails.jsx
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
-import "./styles.css";
+import {useParams, Link, useNavigate} from "react-router-dom";
+import "./CSS/styles.css";
 
 const API_URL = "http://localhost:8080/api/books";
 
 function BookDetails() {
+    const navigate = useNavigate();
     const { id } = useParams();
     const [book, setBook] = useState(null);
 
     useEffect(() => {
-        fetch(`${API_URL}/${id}`)
+        fetch(`${API_URL}/${id}`, {
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("jwtToken")}`,
+            },
+        })
             .then((res) => res.json())
             .then((data) => setBook(data))
             .catch((err) => console.error("Błąd:", err));
@@ -25,7 +30,7 @@ function BookDetails() {
                 <p><strong>Opis:</strong> {book.description}</p>
                 {book.cover && <img src={book.cover} alt="cover_art" width="200" />}
                 <br />
-                <Link to="/" className="back-btn">Powrót do listy</Link>
+                <Link to="/books" className="back-btn">Powrót do listy</Link>
             </div>
         </div>
     );
