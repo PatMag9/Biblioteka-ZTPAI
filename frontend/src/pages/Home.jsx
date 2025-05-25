0ebbfb56023f922394751889b44d4bc8d77040d8
PatMag9 <patryk.magiera@student.pk.edu.pl>
2025-05-25 19:22:21 +0200

    chore: organizing CSS files

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../styles/Home.css";
+import "../styles/HomePages.css";
 import {Link} from "react-router-dom";
 function Home() {
     return(
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 
-import "../styles/Home.css"
+import "../styles/HomePages.css"
 import React, {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import axios from "axios";
